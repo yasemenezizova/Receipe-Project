8f4bca765d11f440564c641f790db1eed9161c04
PROSYS\yasemen.a <yasemen.a@prosys.az>
2023-01-07 01:14:32 +0400

    initial

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingridient } from '../shared/ingridient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingridients:Ingridient[]=[
+    new Ingridient('Apple',5),
+    new Ingridient('Pineapple',15)
+  ];
   constructor() { }
 
   ngOnInit(): void {
